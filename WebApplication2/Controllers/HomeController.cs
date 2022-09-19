7891af5a52141f56719a9ca2dd99d3aa14659853
Anggraini <fitri.anggraini@mattel.com>
2022-09-19 15:17:40 +0700

    Dashboard BOT

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using WebApplication2.Models;
+using System.Data.Entity;
+using System.Threading;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NamlosEntities1 db = new NamlosEntities1();
+        public ActionResult Index()
+        {
+            var Tnest = db.BOT_Toy_Nest.SqlQuery("SELECT * FROM BOT_Toy_Nest").Count();
+            ViewBag.Tnest = Tnest;
+            
+
+            var Tlokator = db.BOT_Lokator_Nest.SqlQuery("SELECT * FROM BOT_Lokator_Nest").Count();
+            ViewBag.Tlokator = Tlokator;
+
+            var NestU = db.BOT_Toy_Nest.SqlQuery("SELECT * FROM BOT_Toy_Nest WHERE Status = 'Dipinjam'").Count();
+            ViewBag.NestU = NestU;
+
+            var tables = new NestModel
+            {
+                bOT_Toy_Nests = db.BOT_Toy_Nest.ToList(),
+                bot_rak_Toys = db.BOT_Rak_Nest.ToList(),
+            };
+
+
+            return View(tables);
+
+            //return View();
+        }
+
+        public ActionResult Member()
+        {
+            var Tmember = db.BOT_Register_Nest.SqlQuery("SELECT * FROM BOT_Register_Nest").Count();
+            ViewBag.Tmember = Tmember;
+
+            var tables = new NestModel
+            {
+                bot_register_Nests = db.BOT_Register_Nest.ToList()
+            };
+
+
+            return View(tables);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/NestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class NestModel
+    {
+        public IEnumerable<BOT_Toy_Nest> bOT_Toy_Nests { get; set; }
+        public IEnumerable<BOT_Rak_Nest> bot_rak_Toys { get; set; }
+        public IEnumerable<BOT_Register_Nest> bot_register_Nests{ get; set; }
+        public IEnumerable<BOT_Lokator_Nest> bot_lokator_Nests { get; set; }
+    }
+}
